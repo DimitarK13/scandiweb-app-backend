9ecd6bec605b490205fb19c5ec5926bd2eba95e9
Dimitar <50843303+DimitarK13@users.noreply.github.com>
2023-02-21 23:27:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Headers: *");
   header("Access-Control-Allow-Methods: *");
+  header("Access-Control-Allow-Origin: *");
 
   include 'DbConnect.php';
   $objDb = new DbConnect;
